62cc24b49a6f510d0986d03693a4f0d168bceb8d
zinovi.rabinovich <zinovi.rabinovich@a4c2d18a-7c58-11de-8963-3b09bc248160>
2012-03-05 15:46:53 +0000

    initial policy calculations

--- trunk/sigma/matlab/f_softmax_exp3_pred.m
+++ trunk/sigma/matlab/f_softmax_exp3_pred.m
@@ -1,4 +1,4 @@
-function [d,d_clear] = f_softmax_pred(pi_mat,params)
+function [d,d_clear] = f_softmax_exp3_pred(pi_mat,params)
 
 L = (params.sigma_w*params.l_user)';
 
@@ -7,11 +7,11 @@ L_star = params.sigma_w*diag(params.l_user*pi_mat);
 d = exp(-params.eta*[L;L_star]);
 d = d./sum(d);
 
-%d = (1-params.gamma)*d+params.gamma*ones(size(d))/length(d);
+d = (1-params.gamma)*d+params.gamma*ones(size(d))/length(d);
 
 if (nargout == 2)
   d_clear = exp(-params.eta*L);
   d_clear = d_clear./sum(d_clear);
-%  d_clear = (1-params.gamma)*d_clear+...
-%	    params.gamma*ones(size(d_clear))/length(d_clear);
+  d_clear = (1-params.gamma)*d_clear+...
+	    params.gamma*ones(size(d_clear))/length(d_clear);
 end
