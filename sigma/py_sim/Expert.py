c69de9e84df7082edf90812a0e69fc6128be4353
zinovi.rabinovich <zinovi.rabinovich@a4c2d18a-7c58-11de-8963-3b09bc248160>
2012-06-19 18:29:28 +0000

    sigma implementation -- will need to expand the comments

--- sigma/py_sim/Expert.py
+++ sigma/py_sim/Expert.py
@@ -1,6 +1,6 @@
 from numpy import *
 import scipy as Sci
-import scipy.linalg
+import scipy.linalg 
 # Ensure you can save Matlab formated trace datafiles
 # from scipy.io import mio
 #import random
@@ -9,6 +9,8 @@ import scipy.linalg
 import sys
 # Ensure basic math is available
 import math
+
+from User import *
 
 class ExpertBase:
     def __init__(self):
@@ -65,8 +67,106 @@ class ExpertBest(ExpertBase):
         self.span = span
 
     def push(self,observation):
-        self.advice=argmin(observation[0:self.span])
+        self.advice=argmin(observation[:self.span])
 
     def next(self):
         return self.advice
    
+class ExpertWorst(ExpertBase):
+    advice = 0;
+    span = 4
+    def __init__(self,span):
+        ExpertBase.__init__(self)
+        self.advice = 0;
+        self.span = span
+
+    def push(self,observation):
+        self.advice=argmax(observation[:self.span])
+
+    def next(self):
+        return self.advice
+   
+class ExpertSelfish(ExpertBase):
+    advice = 0;
+    span = 4
+    def __init__(self,span):
+        ExpertBase.__init__(self)
+        self.advice = 0;
+        self.span = span
+
+    def push(self,observation):
+        self.advice=argmin(observation[self.span:])
+
+    def next(self):
+        return self.advice
+   
+class ExpertSigma(ExpertBase):
+    advice = 0;
+    codex = dict()
+    def __init__(self,policy,user):
+        ExpertBase.__init__(self)
+        self.advice = 0;
+        self.codex = dict()
+        if (policy.has_key('pi')):
+            self.codex['pi']=policy['pi']
+        else:
+            raise KeyError
+        if (policy.has_key('d_star')):
+            self.codex['d_star']=policy['d_star']
+        else:
+            raise KeyError
+        if (policy.has_key('sigma_p')):
+            self.codex['sigma_p']=policy['sigma_p']
+        else:
+            raise KeyError
+        if (policy.has_key('sigma')):
+            self.codex['sigma']=policy['sigma']
+            self.codex['inv_sigma']=scipy.linalg.inv(policy['sigma'])
+        else:
+            raise KeyError
+        self.codex['user']=user
+
+    def push(self,observation):
+        ### Find nearest sigma point
+        # Find differences between sigma points and the observation
+        mat_tmp = self.codex['sigma_p'].T.copy()-observation
+        # Calculate (squares) of difference norms
+        norms = diag(dot(mat_tmp,dot(self.codex['inv_sigma'],mat_tmp.T)))
+        #(mat_tmp*mat_tmp).sum(1)
+        # Index of the nearest sigma point
+        best_sigma = argmin(norms);
+
+        (a_no,p_no) = self.codex['pi'].shape
+
+        ### Calculate the action distribution at the best sigma point
+        q = self.codex['d_star'][0:-1].copy()+\
+            self.codex['d_star'][-1]*self.codex['pi'][:,best_sigma]
+
+        ### Calculate the discrepancy of reproducable distributions
+        # diffs = []
+        # for a_idx in range(a_no):
+        #     d = self.codex['user'].next_d([])
+        #     q_a = d[0:-1].copy()
+        #     q_a[a_idx]=q_a[a_idx]+d[-1]
+        #     v = q-q_a;
+        #     #diffs.append(dot(v,dot(self.codex['inv_sigma'],v)))
+        #     diffs.append(scipy.linalg.norm(v))
+
+        ## Compute the proportion of action that is missing from the
+        ## current point when compared to the closest sigma point
+        d = self.codex['user'].next_d([])
+        q_a_cut = d[0:-1].copy()
+        v = q-q_a_cut
+        advice_distro = v.copy()
+        for a_i in range(a_no):
+            if (v[a_i]<0.0):
+                advice_distro[a_i]=0.0
+        if (advice_distro.sum()<=0):
+            advice_distro = 1.0-v
+        #advice_distro = exp(-1.0*array(diffs))
+        advice_distro = advice_distro/advice_distro.sum()
+        #self.advice = argmin(diffs)
+        self.advice = argmax(random.multinomial(1,advice_distro))
+
+    def next(self):
+        return self.advice
--- sigma/py_sim/stat.m
+++ sigma/py_sim/stat.m
@@ -1,15 +1,28 @@
-exp_no = 75;
-data = load('tmp0.mat');
-max_idx = length(data.choices);
+list_spa_names = {'worst','selfish','best'};
+
+exp_no = 20;
 win_size = 100;
 bins = [0:4];
 
+
+data = load('tmp0.mat');
+%max_idx = length(data.choices);
+
+[max_idx,outcome_len] = size(data.outcome);
 mat_stat = zeros(length(bins),max_idx-win_size,exp_no);
+e_costs_full = zeros(max_idx,exp_no);
+e_costs_smoothed = zeros(max_idx-win_size,exp_no);
 
 for exp_idx = 1:exp_no,
   data = load(strcat('tmp',int2str(exp_idx-1),'.mat'));
+  for idx = 1:max_idx,
+    e_costs_full(idx,exp_idx)=...
+	data.outcome(idx,outcome_len/2+1+...
+		     data.polls(idx,data.choices(idx)+1));
+  end
   for idx = 1:max_idx-win_size,
     mat_stat(:,idx,exp_idx)=hist(data.choices(idx:idx+win_size),bins);
+    e_costs_smoothed(idx,exp_idx)=mean(e_costs_full(idx:idx+win_size,exp_idx));
   end
 end
 
@@ -33,4 +46,14 @@ for line_idx = 1:length(bins),
   plot([1:max_idx-win_size],ms(line_idx,:),styles{line_idx});
   hold on
 end
-legend(labels)
+legend(labels);
+title('Frequencies of expert use');
+
+% Plotting  SPA's costs
+figure
+plot([1:max_idx-win_size],mean(e_costs_smoothed,2));
+title('Smoothed costs');
+figure
+e_mat = cumsum(e_costs_full,1);
+plot([1:max_idx],mean(e_mat,2));
+title('True costs');
